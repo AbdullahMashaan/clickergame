97b6cf92e37a0c92b108aaa3e4a55579da3df1b2
Abdullah Mashaan <eng.aalenezi@gmail.com>
2024-11-17 19:50:12 +0300

    ClickerGame

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [clickCount, setClickCount] = useState(0);
+ 
+
+    const handleClick = () => {
+        if (clickCount === 0) {
+            alert("Let the fun start!");
+        }
+        setClickCount(prevCount => prevCount + 1);
+    };
+
+  
+    return (
+      
+        <div className="App">
+            <img src="https://images.crazygames.com/click-click-clicker_16x9/20240206102255/click-click-clicker_16x9-cover?auto=format,compress&q=75&cs=strip" alt="Game Image" className="top-image" />
+            <h1>Click Counter Game</h1>
+            <div className="click-count">
+                Clicks: {clickCount}
+            </div>
+            <div className="coin-counter">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4ssCnkNWb6Fc1vX-MrriJpGvlqL0qoGROaQ&s" alt="Coin" className="coin-image" />
+                <span>x {clickCount}</span>
+            </div>
+            <button className="red-button" onClick={handleClick}>Click Me!</button>
+            
+            
+        </div>
+    );
+};
 
 export default App;
